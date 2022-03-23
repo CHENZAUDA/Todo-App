d95bd0ed7fd0846dfcfdc88482cc55f8af5e6058
CHEN ZAUDA <czauda@gmail.com>
2022-03-23 20:37:26 +0200

    upload heroku

--- server/index.js
+++ server/index.js
@@ -24,6 +24,12 @@ const taskRoute = require('./routes/taskRoute')
 app.use('/api/tasks', taskRoute)
 
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
 const server = app.listen(PORT, () => {
     console.log(`live on port ${PORT}`)
 })
